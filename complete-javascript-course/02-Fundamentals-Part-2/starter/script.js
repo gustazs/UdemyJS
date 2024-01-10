f546f9df8f94f94e3e7bb8d12866d60a6c556779
Gustavo Cruz <gustavo.cruzsilva310@gmail.com>
2024-01-10 05:58:37 -0300

    Example While Loop

--- complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -336,3 +336,10 @@ while (rep < 6) {
     console.log(`Lifting weights repetition ${rep}`);
     rep++;
 }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`)
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end...');
+}
