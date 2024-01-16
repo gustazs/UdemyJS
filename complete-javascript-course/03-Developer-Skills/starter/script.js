b36689b6818d774765b2a2675228ea9cfb595565
Gustavo Cruz <gustavo.cruzsilva310@gmail.com>
2024-01-16 20:42:18 -0300

    Taking notes

--- complete-javascript-course/03-Developer-Skills/starter/script.js
+++ complete-javascript-course/03-Developer-Skills/starter/script.js
@@ -14,6 +14,33 @@ Como falhar em codar
 - Não ter um objetivo claro
 - Copiar código sem saber como funciona
 - Não reforçar o que aprende fazendo pequenos desafios ou anotações
-- Não praticar
+- Não pratica fora do curso com projetos pessoais
+- Não se conectar com pessoas que também aprendem programação
 
+Corrigindo falhas:
+1. Não ter um objetivo claro
+- Definir um objetivo específico, mensurável, realista e baseado em algum prazo
+- Saber exatamente o por que você está aprendendo programação
+- Imagine um grande projeto que você é capaz de construir
+- Pesquise tecnologias que você precisa e aprenda elas
+
+2. Copiar código sem saber como funciona
+- Entenda o código que você está estudando e digitando
+- Sempre digite o código, não copie e cole!
+
+3. Não reforçar o que aprende fazendo pequenos desafios ou anotações
+- Depois que aprender um novo recurso ou conceito, use imediatamente
+- Faça anotações
+- Desafie a si mesmo praticando com pequenos desafios e exercícios de código.
+- Não se apresse em terminar o curso rápido!
+
+4. Não pratica fora do curso com projetos pessoais
+- Praticar sozinho é a coisa mais importante a se fazer
+- Não é um opção! Sem a prática fora do curso você não vai a lugar nenhum
+- Faça suas próprias ideias de projeto, ou copie site ou aplicativos populares ou somente uma pequena parte dele
+- Não fique preso em tutoriais
+
+5. Não se conectar com pessoas que também aprendem programação
+- Explique conceitos para outras pessoas
+- 
 */
