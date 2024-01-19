a69da5849ae04a75205e7ba5f81eec742f2510a9
Gustavo Cruz <gustavo.cruzsilva310@gmail.com>
2024-01-19 20:04:20 -0300

    Using StackOverflow

--- complete-javascript-course/03-Developer-Skills/starter/script.js
+++ complete-javascript-course/03-Developer-Skills/starter/script.js
@@ -69,3 +69,7 @@ Corrigindo falhas
 
 
 */
+
+// PROBLEM:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
