50b1f82de41c4058734e5e1fe23c8ec6f35f6b27
Gustavo Cruz <gustavo.cruzsilva310@gmail.com>
2024-02-07 11:22:03 -0300

    click event

--- complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,14 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 21;
+
+document.querySelector('.guess').value = 14 
+*/
+
+document.querySelector('.check').addEventListener('click', function {
+    console.log(document.querySelector('.guess').value);
+});
