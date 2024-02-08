3e1a8e0a324ecc705b9a85fcb77f59aa393cf900
Gustavo Cruz <gustavo.cruzsilva310@gmail.com>
2024-02-08 11:14:13 -0300

    game logic

--- complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -9,6 +9,26 @@ document.querySelector('.score').textContent = 21;
 document.querySelector('.guess').value = 14 
 */
 
-document.querySelector('.check').addEventListener('click', function {
-    console.log(document.querySelector('.guess').value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
 });
